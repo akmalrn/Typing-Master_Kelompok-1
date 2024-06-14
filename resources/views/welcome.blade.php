79bbe601bee3e0f927586bf98e507c4b71454e9a
Dhiksa Raja Nararya Hakim <tshiba257@gmail.com>
2024-06-15 02:04:35 +0700

    dhiks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -234,9 +234,6 @@
                 <!-- Ikon Media Sosial -->
                 <div class="social-icons">
                     <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
                 </div>
 
                 <span>or use your email for registration</span>
