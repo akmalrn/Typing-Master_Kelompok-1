5401a9928f784e689e72bf52f1ded071840d3d45
akmalrn <legendsmystic60@gmail.com>
2024-06-16 22:28:04 +0700

    UpdateMiddlewelare

--- routes/web.php
+++ routes/web.php
@@ -76,8 +76,13 @@ Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallba
 
 //AdminController
 Route::get('admin/HalamanDev',[admin_controller::class, 'HalamanDev'])->name('HalamanDev');
-Route::get('admin/HalamanAdmin', [admin_controller::class, 'HalamanAdmin'])->name('HalamanAdmin');
+Route::get('admin/HalamanAdmin', [admin_controller::class, 'HalamanAdmin'])->middleware('admin')->name('HalamanAdmin');
 Route::get('admin/HalamanReadText', [admin_controller::class, 'HalamanReadText'])->name('HalamanReadText');
 
 
 //middlewelare
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/HalamanAdmin', [admin_controller::class, 'HalamanAdmin'])->name('HalamanAdmin');
+    Route::get('/user/dashboard', [user_controller::class, 'HalamanDashboard'])->name('HalamanDashboard');
+    // Tambahkan rute admin lainnya di sini
+});
