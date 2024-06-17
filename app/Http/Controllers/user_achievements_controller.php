f738c090ea98ab5c5af3f0ce6a1d37e715c0fe25
akmalrn <legendsmystic60@gmail.com>
2024-06-17 23:41:19 +0700

    Perbaruan

--- app/Http/Controllers/user_achievements_controller.php
+++ app/Http/Controllers/user_achievements_controller.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\typing_lessons;
 use Illuminate\Http\Request;
 
 class user_achievements_controller extends Controller
 {
-    //
+    public function HalamanAchievements()
+    {
+        $typing_lessons = typing_lessons::all();
+        return view('user/UserAchievements', compact('typing_lessons'));
+    }
 }
